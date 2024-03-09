e590e0263790fe629019341e19ed47d78141698e
doridori238! <dkffps238@gmail.com>
2024-03-10 00:46:45 +0900

    CSV 연결
    아이템 데이터 연결

--- Assets/Script/DataLoad.cs
+++ Assets/Script/DataLoad.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Transactions;
+using Unity.VisualScripting;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -15,88 +14,69 @@ public class Parser
         TextAsset loadData = Resources.Load<TextAsset>(path);
         string allText = loadData.text;
         string[] lines = allText.Split('\n');
-        
         Item items = new Item();
-        //int itemSwtich = 0;
-
-        Dictionary<int, string> itemDic = new Dictionary<int, string>();
-        //items.itemindex = int.Parse(lines[0]);
-        CultureInfo cultureInfo = CultureInfo.InvariantCulture;
-        for (int i = 1; i < lines.Length; i++)
-        {
-            string[] values = lines[i].Split(",");
-            //int value;
-            // 파서 이유 
-            for (int j = 0; j < values.Length; j++)
-            { 
-              items.itemindex = int.Parse(values[j]);
+       
+        for (int i = 1; i < lines.Length - 1; i++)
+        {            
+            string[] values = lines[i].Split(",");  
+            //if(들어오는 정보로 스위치문으로 해서 담든가 아니면 if문으로 잡아서 처리하면 될것이다.)
+            items = Input(values);       
+            valuesList.Add(items);
                 
-              itemDic.Add(items.itemindex, lines[i]);            
-            }
-            
-            //switch(itemSwtich)
-            //{
-            //    case 0:
-            //        items.itemindex = int.Parse(values[0]);
-            //        break;
-            //    case 1:
-            //        items.itemname = values[1];
-            //        break;
-            //    case 2:
-            //        items.itemtype = int.Parse(values[2]);
-            //        break;
-            //}
-
-            ////string[] values = lines[i].Split(",");
-            //itemDic.Add(items.itemindex, int.Parse(items[i]));
-            //items =  int.Parse(lines[0]);
-            //valuesList.Add(items);
         }
-        valuesList.Add(items);
         return valuesList;
-
 
     }
 
-
-
-
-
+    public static Item Input(string[] values)
+    {  
+        Item item = new Item();
+        
+        item.itemindex = int.Parse(values[0]);
+        item.itemname = values[1];
+        item.itemtype = int.Parse(values[2]);
+        item.detail = int.Parse(values[3]);
+        item.lvlimit = int.Parse(values[4]);
+        item.option1 = int.Parse(values[5]);
+        item.value1 = int.Parse(values[6]);
+        item.option2 = int.Parse(values[7]);
+        item.value2 = int.Parse(values[8]);
+        item.option3 = int.Parse(values[9]);
+        item.value3 = int.Parse(values[10]);
+        item.option4 = int.Parse(values[11]);
+        item.value4 = int.Parse(values[12]);
+       
+        return item;
+    }
+            
 }
 
 
 public class DataLoad : Singleton<DataLoad>
 {
     string path = "ITEM_INFO";
-    Item items = new Item();
-    Dictionary<List<string[]>, Item> itemDic = new Dictionary<List<string[]>, Item>();
-
+    Dictionary<int, Item> itemDic = new Dictionary<int, Item>();
+    Item item1;
     void Start()
-    {
+    {   
         List<Item> valuesList  =  Parser.Parse(path);
-    
-        //Debug.Log(valuesList.Count.ToString());
-        //foreach (string[] values in valuesList)
-        //{
 
-        //    itemDic.Add(values[i], items);
-            
-        //}
-        //Debug.Log(itemDic.);
+        for (int i = 0; i < valuesList.Count; i++)
+            itemDic.Add(valuesList[i].itemindex, valuesList[i]);
 
+        item1 = itemDic[1];
+        Debug.Log(item1.itemname);
 
         //딕션어리 통한 키 벨류로 만들기
+
     }
 
-
-    
 }
 
 
 
 public struct Item
 {
-
     public int itemindex;
     public string itemname;
     public int itemtype;
@@ -110,7 +90,6 @@ public struct Item
     public int value3;
     public int option4;
     public int value4;
-
 }
 
 ///// <summary>
